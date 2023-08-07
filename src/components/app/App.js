7ff364e6ad27543533c22034c883845e866f793f
Zuli <114097622+ZulfiyaKurmanaeva@users.noreply.github.com>
2023-08-07 09:18:41 +0300

    context

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,43 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Main from '../main/Main';
 import Movies from '../movies/Movies';
 import Register from '../register/Register';
 import Login from '../login/Login';
 import Profile from '../profile/Profile';
 import NotFoundPage from '../notFoundPage/NotFoundPage';
+import CurrentUserContext from "./CurrentUserContext";
+import {useEffect, useState} from "react";
+import {getUser} from "../../utils/MainApi";
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const i = setInterval(() =>
+    setLoggedIn(localStorage.getItem("jwt") !== null), 50);
+    getUser().then(setUser);
+  }, []);
   
   return (
     <div className="page">
+      <CurrentUserContext.Provider value={user}>
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/movies' element={<Movies saved={false}/>} />
-        <Route path='/saved-movies' element={<Movies saved={true}/>} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/movies' element={<ProtectedRoute loggedIn={loggedIn}><Movies saved={false}/></ProtectedRoute>} />
+        <Route path='/saved-movies' element={<ProtectedRoute loggedIn={loggedIn}><Movies saved={true}/></ProtectedRoute>} />
+        <Route path='/profile' element={<ProtectedRoute loggedIn={loggedIn}><Profile /></ProtectedRoute>} />
         <Route path='/signup' element={<Register />} />
         <Route path='/signin' element={<Login />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
+      </CurrentUserContext.Provider>
     </div>
   );
+}
+
+function ProtectedRoute({loggedIn, children}){
+  return loggedIn ? <>{children}</> : <NotFoundPage />;
 }
 
 /*export default function App() {
--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,3 +1,6 @@
 const API_URL = "https://api.nomoreparties.co/beatfilm-movies";
 
-export const getAllMovies = () => fetch(API_URL).then(x => x.json());
+export async function getAllMovies(){
+    const r = await fetch(API_URL);
+    return await r.json();
+}
