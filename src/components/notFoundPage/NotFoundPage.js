ec0094bfd8f731ade495f4585f3e1a850a55736e
Zuli <114097622+ZulfiyaKurmanaeva@users.noreply.github.com>
2023-08-08 09:50:49 +0000

    not found fixed

--- src/components/notFoundPage/NotFoundPage.js
+++ src/components/notFoundPage/NotFoundPage.js
@@ -1,13 +1,19 @@
 import './NotFoundPage.css';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
     return (
       <main>
       <section className='not-found'>
         <h1 className='not-found__title'>404</h1>
         <p className='not-found__subtitle'>Страница не найдена</p>        
-        <Link to='/' className='not-found__link'>Назад</Link>
+        <button type='button' onClick={ handleReturn } className='not-found__button'>Назад</button>
       </section>
       </main>   
   );
